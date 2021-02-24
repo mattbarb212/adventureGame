69d3e49c232bd6b2d9fe87c0aa226f8544398d39
Matthew Barber <mattbarb212@outlook.com>
2021-02-24 14:42:16 -0500

    Add project files.

---
+++ adventureGame/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace adventureGame
+{
+    partial class adventureGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(adventureGame));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.redButton = new System.Windows.Forms.PictureBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.Location = new System.Drawing.Point(12, 13);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(483, 62);
+            this.outputLabel.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::adventureGame.Properties.Resources.Green_Button;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(241, 335);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 43);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::adventureGame.Properties.Resources.Blue_Button;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 391);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(45, 43);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // redButton
+            // 
+            this.redButton.BackgroundImage = global::adventureGame.Properties.Resources.Red_Button;
+            this.redButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.redButton.Location = new System.Drawing.Point(12, 335);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(45, 43);
+            this.redButton.TabIndex = 2;
+            this.redButton.TabStop = false;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 79);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(483, 233);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // redLabel
+            // 
+            this.redLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.Location = new System.Drawing.Point(64, 335);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(171, 43);
+            this.redLabel.TabIndex = 5;
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenLabel.Location = new System.Drawing.Point(292, 335);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(171, 43);
+            this.greenLabel.TabIndex = 6;
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.Location = new System.Drawing.Point(64, 391);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(171, 43);
+            this.blueLabel.TabIndex = 7;
+            // 
+            // adventureGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 446);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.outputLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "adventureGame";
+            this.Text = "Adventure Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AdventureGame_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.redButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox redButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label blueLabel;
+    }
+}
+
---
+++ adventureGame/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adventureGame
+{
+    public partial class adventureGame : Form
+    {
+        int scene = 0;
+        public adventureGame()
+        {
+            InitializeComponent();
+        }
+
+        private void AdventureGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0)
+                {
+                    scene = 2;
+                }
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+                else if (scene == 2)
+                {
+                    scene = 25;
+                }
+                else if (scene == 3)
+                {
+                    scene = 6;
+                }
+                else if (scene == 4)
+                {
+                    scene = 6;
+                }
+                else if (scene == 5)
+                {
+                    scene = 8;
+                }
+                else if (scene == 6)
+                {
+                    scene = 25;
+                }
+                else if (scene == 7)
+                {
+                    scene = 9;
+                }
+                else if (scene == 8)
+                {
+                    scene = 25;
+                }
+                else if (scene == 9)
+                {
+                    scene = 12;
+                }
+                else if (scene == 10)
+                {
+                    scene = 12;
+                }
+                else if (scene == 11)
+                {
+                    scene = 25;
+                }
+                else if (scene == 12)
+                {
+                    scene = 15;
+                }
+                else if (scene == 13)
+                {
+                    scene = 25;
+                }
+                else if (scene == 14)
+                {
+                    scene = 17;
+                }
+                else if (scene == 15)
+                {
+                    scene = 25;
+                }
+                else if (scene == 16)
+                {
+                    scene = 25;
+                }
+                else if (scene == 17)
+                {
+                    Random randGen = new Random();
+                    int value = randGen.Next(1, 101);
+
+                    if (value < 20) { scene = 21; }
+                    else { scene = 22; }
+                }
+                else if (scene == 18)
+                {
+                    scene = 25;
+                }                
+                else if (scene == 20)
+                {
+                    scene = 23;
+                }
+                else if (scene == 21)
+                {
+                    scene = 23;
+                }
+                else if (scene == 22)
+                {
+                    scene = 25;
+                }
+                else if (scene == 23)
+                {
+                    scene = 25;
+                }
+                else if (scene == 24)
+                {
+                    scene = 25;
+                }
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+
+                if (scene == 0) { scene = 1; }
+                else if (scene == 1) { scene = 3; }
+                else if (scene == 2) { scene = 0; }
+                else if (scene == 3) { scene = 5; }
+                else if (scene == 4) { scene = 5; }
+                else if (scene == 5) { scene = 7; }
+                else if (scene == 6) { scene = 0; }
+                else if (scene == 7)
+                {
+                    Random randGen = new Random();
+                    int value = randGen.Next(1, 101);
+
+                    if (value < 50) { scene = 11; }
+                    else { scene = 10; }
+                }
+                else if (scene == 8) { scene = 0; }
+                else if (scene == 9) { scene = 13; }
+                else if (scene == 10) { scene = 13; }
+                else if (scene == 11) { scene = 0; }
+                else if (scene == 12) { scene = 14; }
+                else if (scene == 13) { scene = 0; }
+                else if (scene == 14) { scene = 18; }
+                else if (scene == 15) { scene = 0; }
+                else if (scene == 16) { scene = 0; }
+                else if (scene == 17) { scene = 20; }
+                else if (scene == 18) { scene = 0; }                
+                else if (scene == 20) { scene = 24; }
+                else if (scene == 21) { scene = 24; }
+                else if (scene == 22) { scene = 0; }
+                else if (scene == 23) { scene = 0; }
+                else if (scene == 24) { scene = 0; }
+            }
+            else if (e.KeyCode == Keys.Space)  //green button press
+            {
+                if (scene == 14) { scene = 16; }
+            }
+
+            /// Display text and game options to screen based on the current scene
+            switch (scene)
+            {
+                case 0:  //start scene  
+                    outputLabel.Text = "You are in a hallway with a door to the left and the right which one do you want to go through?";
+                    redLabel.Text = "Right";
+                    blueLabel.Text = "Left";
+                    break;
+                case 1:
+                    outputLabel.Text = "You walk through the door and a voice says, Welcome, Do you want to play a game";
+                    redLabel.Text = "No";
+                    blueLabel.Text = "Yes";
+                    break;
+                case 2:
+                    outputLabel.Text = "You walk through the door and you exit to the city, Yay you escaped do you want to play again?";
+                    redLabel.Text = "No";
+                    blueLabel.Text = "Yes";
+                    break;
+                case 3:
+                    outputLabel.Text = "The walls of the room start to close and a voice tells you to enter but remember that the truth may not be reality. Do you walk through?";
+                    redLabel.Text = "No";
+                    blueLabel.Text = "Yes";
+                    break;
+                case 4:
+                    outputLabel.Text = "A voice says too bad and laughs, the walls start to close in and a door opens. Do you walk through?";
+                    redLabel.Text = "No";
+                    blueLabel.Text = "Yes";
+                    break;
+                    //Ended the day here
+                case 5:
+                    outputLabel.Text = "You tame the horse and ride to safety, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 6:
+                    outputLabel.Text = "The horse swims by and you die of loneliness, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 7:
+                    outputLabel.Text = "The horse swims by and you die of loneliness, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 8:  
+                    outputLabel.Text = "You are lost in a forest which way do you want to go";
+                    redLabel.Text = "North";
+                    blueLabel.Text = "South";
+                    break;
+                case 9:
+                    outputLabel.Text = "You come to a lake do you want to drink";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 10:
+                    outputLabel.Text = "You fall in a pit and die, Play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 11:
+                    outputLabel.Text = "The water is stagent, you die of Cholera, Play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 12:
+                    outputLabel.Text = "A horse swims by do you try to ride it";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 13:
+                    outputLabel.Text = "You tame the horse and ride to safety, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 14:
+                    outputLabel.Text = "The horse swims by and you die of loneliness, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 15:
+                    outputLabel.Text = "The horse swims by and you die of loneliness, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 16:   
+                    outputLabel.Text = "You are lost in a forest which way do you want to go";
+                    redLabel.Text = "North";
+                    blueLabel.Text = "South";
+                    break;
+                case 17:
+                    outputLabel.Text = "You come to a lake do you want to drink";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 18:
+                    outputLabel.Text = "You fall in a pit and die, Play again?";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;               
+                case 20:
+                    outputLabel.Text = "A horse swims by do you try to ride it";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 21:
+                    outputLabel.Text = "You tame the horse and ride to safety, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 22:
+                    outputLabel.Text = "The horse swims by and you die of loneliness, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 23:
+                    outputLabel.Text = "The horse swims by and you die of loneliness, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 24:
+                    outputLabel.Text = "The horse swims by and you die of loneliness, Play again";
+                    redLabel.Text = "Yes";
+                    blueLabel.Text = "No";
+                    break;
+                case 25:
+                    outputLabel.Text = "Thanks for playing";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+
+
+
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
